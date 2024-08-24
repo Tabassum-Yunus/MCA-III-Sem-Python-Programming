ab447bf298e69b8c4f350441d653f09d6bddef33
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-08-24 19:33:17 +0530

    Sum of first 10 integers

---
+++ Week 1/Q2.py
@@ -0,0 +1,12 @@
+'''     Write a program to print sum of first 10 numbers.     '''
+
+# initialize a variable to store the sum of numbers
+sum = 0
+
+# Loop through the first 10 natural numbers from 1 to 10
+for i in range(1,11):
+    # Add the current number to the sum
+    sum += i
+
+# Print the sum of first 10 natural numbers
+print("Sum of first 10 natural numbers: ", sum)
