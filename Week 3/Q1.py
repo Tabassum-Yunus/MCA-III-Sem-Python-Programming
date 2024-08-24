89962e766af9b0ed290f86cfac8f23ed35e36e76
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-08-24 19:56:26 +0530

    Dowrward right triangle number sequence pattern

--- Week 3/Q1.py
+++ Week 3/Q1.py
@@ -1,6 +1,6 @@
 
 '''     Write a program to print the following pattern using the for loop:
-                    5 4 3 2 1
+                    5 4 3 2 1 
                     4 3 2 1
                     3 2 1
                     2 1
