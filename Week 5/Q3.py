9fa9442547a2497cbe9b31f84a2dbbc0faecc152
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-09-25 18:15:20 +0530

    Generate a password of length 10 must contain at least 2 uppercase letters, 1 digit, and 1 special symbol.

--- Week 5/Q3.py
+++ Week 5/Q3.py
@@ -1,24 +1,37 @@
-import random
-import string
+'''     3. Write a program to generate a random Password which meets the following conditions
+            a. Password length must be 10 characters long.
+            b. It must contain at least 2 upper case letters, 1 digit, and 1 special symbol.    '''
 
-# def generate_pswd():
-#     up_letters = random.choices(string.ascii_uppercase,k=2)
-#     digit = random.choices(string.digits,k=1)
-#     spsym = random.choices(string.punctuation,k=1)
-#     others = random.choices(string.ascii_letters + string.digits + string.punctuation,k=6)
-#     password_list = up_letters + digit + spsym+ others
-#     # random.shuffle(password_list)
-    
-#     # return ''.join(password_list)
-#     print(password_list)
-    
-# print("Password:", generate_pswd())
 
-'''
-string.ascii_lowercase: (‘a’ to ‘z’)
-string.ascii_letters: (string.ascii_lowercase + string.ascii_uppercase)
-string.digits: (string ‘0123456789’)
-string.punctuation: (string of ASCII characters that are considered punctuation characters)
-string.whitespace: (includes characters like space, tab, linefeed, return, formfeed, and vertical tab)
-string.printable: (digits, letters (both uppercase and lowercase), punctuation, and whitespace characters)
-'''
+# Import the random module for generating random characters
+import random  
+
+# Generate one random special symbol
+symbol = [chr(random.randrange(33, 47))]  # ASCII values for special symbols from '!' (33) to '/' (46)
+
+# Generate one random digit
+digit = [chr(random.randrange(48, 58))]  # ASCII values for digits from '0' (48) to '9' (57)
+
+upper = []  # List to store uppercase letters
+lower = []  # List to store lowercase letters
+
+# Generate two random uppercase letters
+for i in range(2):
+    upper.append(chr(random.randrange(65, 91)))  # ASCII values for uppercase letters from 'A' (65) to 'Z' (90)
+
+# Generate six random lowercase letters
+for i in range(6):
+    lower.append(chr(random.randrange(97, 123)))  # ASCII values for lowercase letters from 'a' (97) to 'z' (122)
+
+# Combine all characters into a single password list
+password = symbol + digit + upper + lower
+
+# Shuffle the password list to randomize the order of characters
+random.shuffle(password)
+
+# Convert the list of characters into a string
+password = ''.join(password)
+
+# Print the generated password
+print(password)
+
