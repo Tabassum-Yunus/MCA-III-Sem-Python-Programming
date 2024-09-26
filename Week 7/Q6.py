569315e0ec1372f409d5467c330b61c59e0390b6
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-09-26 12:11:29 +0530

    Swap the middle line of one file with the last line of second file

---
+++ Week 7/Q6.py
@@ -0,0 +1,30 @@
+'''     Consider two files having some lines of statements. Write a Python program to swap content 
+        present at middle line of first file with the content of last line of the second file. 
+        (Note: First file contains odd numbers of lines of statement)     '''
+
+fh1 = open('File1_W7Q6.txt', 'r+')
+fh2 = open('File2_W7Q6.txt', 'r+')
+
+f1_lines = fh1.readlines()
+f2_lines = fh2.readlines()
+print('BEFORE SWAPPIPNG')
+print('Content of file 1: ',f1_lines)
+print('Content of file 2: ',f2_lines)
+
+fh1.close()
+fh2.close()
+
+fh1 = open('File1_W7Q6.txt', 'w')
+fh2 = open('File2_W7Q6.txt', 'w')
+
+f1_lines[len(f1_lines)//2], f2_lines[len(f2_lines)-1] = f2_lines[len(f2_lines)-1], f1_lines[len(f1_lines)//2]
+
+print('AFTER SWAPPIPNG')
+print('Content of file 1: ',f1_lines)
+print('Content of file 2: ',f2_lines)
+
+fh1.writelines(f1_lines)
+fh2.writelines(f2_lines)
+
+fh1.close()
+fh2.close()
