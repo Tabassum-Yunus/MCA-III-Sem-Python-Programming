6c13b8ca3b0bff588fe8908ae62cff4b9987d762
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-09-26 10:38:28 +0530

    Factorial of an integer

--- Github/Q14 sum of digits of number.py
+++
@@ -1,9 +0,0 @@
-'''     Write a Python function to calculate the sum of digits of a given integer.      '''
-
-num = int(input('Enter an integer: '))
-sum = 0
-while(num != 0):
-    rem = num%10
-    sum += rem
-    num //= 10
-print(sum)
--- Week 7/Q5.py
+++
@@ -1,3 +0,0 @@
-'''     Consider two files that contain information about Employees and Departments in the following parameters: 
-        Employee (Name, EId, Salary, DID), Department (DID, DName, DLocation). Write a Python program to find 
-        the average salary of each department.      '''
