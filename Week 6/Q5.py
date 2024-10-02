15df1aced75aa94afbfa36a2acd3067378e46ea8
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-10-02 08:53:20 +0530

    Write user input names, roll nos, marks of students in a file.

---
+++ Week 6/Q5.py
@@ -0,0 +1,17 @@
+'''     Write a program to get roll numbers, names and marks of the students of a class
+        (get from user) and store these details in a file called “Marks.data”.'''
+
+fh = open('D:\MyVScodeProgs\Programs\Python\Week 6\File_W6Q5.txt', 'w')
+print('Enter name, roll no and marks of strudents')
+
+fh.write('NAME \t ROLL NO. \t MARKS\n')
+while(True):
+    name = input('Name: ')
+    roll_no = input('Roll No: ')
+    marks = list(input('Mark(s): ').split())
+    marks = ', '.join(map(str, marks))
+    fh.write(name+' \t '+roll_no+' \t\t'+marks+'\n')
+    inp = input('Want to Quit? Y/N: ')
+    if(inp == 'y' or inp == 'Y'):
+        break
+fh.close()
