c198ef9e929dbf4bb2c2197e319acec9800b7521
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-10-04 20:42:33 +0530

    DFA for strings starting with 'ab'

---
+++ Extras/DFAs/DFA for ab as prefix.py
@@ -0,0 +1,34 @@
+
+states ={'q0', 'q1', 'q2', 'q3'}
+alphabet = {'a', 'b'}
+transition = {
+        'q0': {'a': 'q1', 'b': 'q3'},
+        'q1': {'a': 'q3', 'b': 'q2'},
+        'q2': {'a': 'q2', 'b': 'q2'},
+        'q3': {'a': 'q3', 'b': 'q3'}
+}
+
+initial_state = 'q0'
+accepting_states = {'q2'}
+
+def simulate_dfa(input_string):
+    current_state = initial_state
+    for symbol in input_string:
+        if symbol not in alphabet:
+            return False
+        current_state = transition[current_state][symbol]
+
+    if current_state in accepting_states:
+        return True
+    return False
+
+
+input_strings = ['001', 'bbba', 'aab', 'aba', 'abaaba', 'ab']
+for string in input_strings:
+    if(simulate_dfa(string)):
+        print(string,'is ACCEPTED')
+    else:
+        print(string,'is REJECTED')
+        
+        
+        
