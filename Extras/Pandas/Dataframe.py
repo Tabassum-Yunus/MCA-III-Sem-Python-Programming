5006cdeaaa57b222b03c16f3758b312fbeb278ef
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-10-04 20:18:05 +0530

    Create a Data Frame in pandas

---
+++ Extras/Pandas/Dataframe.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    'Name': ['Jerry', 'Denny', 'Cookie'],
+    'Status': ['Expired', 'Alive', 'Alive']
+}
+df = pd.DataFrame(data)
+# print(df)
+df.index = [chr(i+65) for i in range(3)]
+# print(df)
+
+data1 = np.random.randint(1,10,(10,10))   # creating an array of 6x5
+df1 = pd.DataFrame(data1)               # convert the array into dataframe
+df1.index = ['R'+str(i) for i in range(10)]  # assign row indices
+df1.columns = ['C'+str(i) for i in range(10)]    # assign column indices
+
+print(df1)  # print the whole dataframe
+
+print(df['C4'])
